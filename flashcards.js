ed849edb2a704eef6041c8eab4236e7b828908ad
Johnsen02 <csen@newroads.org>
2018-01-26 09:41:05 -0800

    made adding thing woprk

---
+++ flashcards.js
@@ -0,0 +1,11 @@
+//addition operation
+function addNumbers(){
+
+	var operand1 = document.getElementById("operand1").value
+	var operand2 = document.getElementById("operand2").value
+
+	var sum = parseInt(operand1) + parseInt(operand2);
+
+	alert(sum);
+
+}
